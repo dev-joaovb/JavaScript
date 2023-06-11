55842447401600fe2f0acc543356270d67da36dc
João Victor Bueno Carneiro <jvbueno12.jobo@hotmail.com>
2023-06-11 13:51:44 -0300

    Aula Arrays (básico)

---
+++ JavaScript Básico/Aula 13/Arrays.js
@@ -0,0 +1,39 @@
+///             0        1        2         3        4
+let alunos = ['Luiz', 'Maria', 'João']; // Luiza, Eduardo
+alunos = 123;
+
+
+console.log(typeof alunos); /// tipo de dado da variável
+console.log(alunos instanceof Array); /// Informa se a variavel é uma instancia Array
+
+// alunos.push('Luiza');
+// alunos.push('Eduardo');
+
+// console.log(alunos.slice(0, -2));
+
+// console.log(alunos[50]);
+
+// delete alunos[1];
+// console.log(alunos[1]);
+
+// const removido = alunos.shift();
+// console.log(removido);
+// console.log(alunos);
+
+// alunos.unshift('Fabio'); // Adiciona no começo
+// alunos.unshift('Luiza'); 
+
+// alunos.push('Luiza'); // Adiciona no fim
+// alunos.push('Fabio'); 
+
+// alunos[alunos.length] = 'Luiza'; //Adicionada no fim
+// alunos[alunos.length] = 'Fabio';
+// alunos[alunos.length] = 'Luana';
+
+
+// alunos[0] = 'Eduardo'; // Altera
+// alunos[3] = 'Luiza'; // Adicionado
+
+// console.log(alunos);
+// console.log(alunos[0]);
+// console.log(alunos[2]);
